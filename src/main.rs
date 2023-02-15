accac69353e701a01880b8eca63a098dda0d3700
Alexandria <alxpettit@gmail.com>
2023-02-15 16:43:05 -0700

    idk

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use derive_more::Display;
 use opencv::core::{Point, VecN};
 use opencv::highgui::WND_PROP_VISIBLE;
 use opencv::imgcodecs::IMREAD_COLOR;
@@ -7,20 +8,45 @@ use opencv::viz::Color;
 use opencv::{highgui, imgcodecs, Result};
 use snafu::{prelude::*, Error, Whatever};
 use std::fmt::format;
+use std::fmt::Display;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::thread;
 use std::time::{Duration, Instant};
 
-static WINDOW_NAME: &str = "TEST";
-
-fn win_visible() -> Result<bool, Whatever> {
-    let win_visible_prop = highgui::get_window_property(WINDOW_NAME, WND_PROP_VISIBLE)
-        .with_whatever_context(|_| {
-            format!("Could not retrieve window property from OpenCV API to check visibility")
-        })?;
-    Ok(win_visible_prop != 0.)
+#[derive(Debug, Display)]
+struct Window {
+    name: &'static str,
 }
+
+impl From<Window> for &str {
+    fn from(value: Window) -> Self {
+        &value.name
+    }
+}
+
+impl Window {
+    fn mkwin(&self, flags: i32) -> Result<(), Whatever> {
+        highgui::named_window(WINDOW.name, flags)
+            .with_whatever_context(|_| format!("Failed to create window: {}", WINDOW.name))?;
+        Ok(())
+    }
+    fn win_visible(&self) -> Result<bool, Whatever> {
+        let win_visible_prop = highgui::get_window_property(&self.name, WND_PROP_VISIBLE)
+            .with_whatever_context(|_| {
+                format!("Could not retrieve window property from OpenCV API to check visibility")
+            })?;
+        Ok(win_visible_prop != 0.)
+    }
+
+    fn win_display_frame(&self, mat: &dyn opencv::core::ToInputArray) -> Result<(), Whatever> {
+        highgui::imshow(WINDOW.name, mat)
+            .with_whatever_context(|_| format!("Could not render next frame"))?;
+        Ok(())
+    }
+}
+
+static WINDOW: Window = Window { name: "TEST" };
 
 fn main() -> Result<(), Whatever> {
     let image = imgcodecs::imread("twily.png", IMREAD_COLOR)
@@ -41,8 +67,7 @@ fn main() -> Result<(), Whatever> {
     )
     .with_whatever_context(|_| format!("Failed to put text."))?;
 
-    highgui::named_window(WINDOW_NAME, WND_PROP_VISIBLE)
-        .with_whatever_context(|_| format!("Failed to create window: {}", WINDOW_NAME))?;
+    WINDOW.mkwin(WND_PROP_VISIBLE)?;
 
     let show_uwu = Arc::new(AtomicBool::new(false));
     let show_uwu_ptr = show_uwu.clone();
@@ -53,15 +78,11 @@ fn main() -> Result<(), Whatever> {
         thread::sleep(Duration::from_secs(1));
     });
 
-    'main_loop: while win_visible()? {
-        highgui::imshow(
-            WINDOW_NAME,
-            match show_uwu.load(Ordering::SeqCst) {
-                true => &image_uwu,
-                false => &image,
-            },
-        )
-        .unwrap();
+    'main_loop: while WINDOW.win_visible()? {
+        WINDOW.win_display_frame(match show_uwu.load(Ordering::SeqCst) {
+            true => &image_uwu,
+            false => &image,
+        })?;
         let key = highgui::wait_key(1).expect("Oops");
         if key == 'q' as i32 {
             break 'main_loop;
